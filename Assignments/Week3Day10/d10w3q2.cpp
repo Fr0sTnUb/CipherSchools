bad8ba7676784cb10d263c8a060f4ad8fb3baff1
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2024-09-18 20:32:31 +0530

    Day 10 ques 1, 2

--- Assignments/Week3Day10/d10w3q2.cpp
+++ Assignments/Week3Day10/d10w3q2.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-bool isPalindrome(const string &str){
-    int i = 0;
-    char arr[100], size = 0;
-    while(str[i] != NULL){
-        if((str[i] >= 97 && str[i] <=122) || (str[i] >= 48 && str[i] <= 57))
-            arr[size++] = str[i];
-        else if(str[i] >= 65 && str[i] <= 90)
-            arr[size++] = str[i] + 32;
-        ++i;
+bool isPalindrome(string &str1){
+    int left = 0;
+    int right = str1.length() - 1;
+
+    while(left < right){
+        while(left < right && ! isalpha(str1[left]) && !isdigit(str1[left])){
+            left++;
+        }
+        while(left < right && ! isalpha(str1[right]) && !isdigit(str1[right])){
+            right--;
+        }
+
+        if(tolower(str1[left]) != tolower(str1[right])){
+            return false;
+        }
+
+        left++;
+        right--;
     }
-    int j = 0, k = size - 1;
-    while(j < k){
-        if (arr[j] != arr[k])
-            break;
-        ++j;
-        --k;
-    }
-    if (j >= k)
-        return true;
-    else
-        return false;
+
+    return true;
 }
 
+
 int main(){
-    string s1;
-    cout << "Enter a string: ";
-    getline(cin, s1);
-    if (isPalindrome(s1))
-        cout << "true";
-    else
-        cout << "false";
-    return 0;
+    
+    string str1 = "A man, a plan, a canal, Panama";
+
+    cout << isPalindrome(str1);
+   return 0;
 }
