b2baf30cb5e2f89aab728821cf7745e39e76f6f2
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2024-10-01 00:17:31 +0530

    Day 18 Week 5 Question 1

---
+++ Assignments/Week5Day18/d18w5q1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Person{
+    public:
+        string firstName;
+        string lastName;
+        unsigned age;
+    Person(){
+        cout << "Enter first name: ";
+        cin >> this -> firstName;
+        cout << "Enter last name: ";
+        cin >> this -> lastName;
+        cout << "Enter age: ";
+        cin >> this -> age;
+    }
+    string getFullName(){
+        return firstName + " " + lastName + "\n";
+    }
+    tuple<string, string, unsigned>getDetails(){
+        return make_tuple(firstName, lastName, age);
+    }
+};
+
+int main(){
+    Person p;
+    cout << p.getFullName();
+    string f, l;
+    unsigned a;
+    tie(f, l, a) = p.getDetails();
+    cout << "First Name: " << f << endl;
+    cout << "Last Name: " << l << endl;
+    cout << "Age: " << a << endl;
+    return 0;
+}
